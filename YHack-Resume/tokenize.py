100831507e97b758aee7826ffc8b529612ba7780
Ashley Wong <ashleywo@andrew.cmu.edu>
2015-11-07 11:51:13 -0500

    tokenize works

--- YHack-Resume/tokenize.py
+++ YHack-Resume/tokenize.py
@@ -1,18 +1,4 @@
-# Given input text, return next word
-def next_word(input_text):
-  # code goes here
-
-
-
 # Given input text, return list with tokens
 def input_file(input_text, tokens):
-  if(input_text == ""):
-    return tokens
-  word = next_word(input_text)
-  tokens.append(word)
-  input_text = input_text[len(word)-1:] # take out the word from text
-  if(input_text == ""): # if that was the last word
-    return tokens
-  else:
-    input_text = input_text[1:] # get rid of the whitespace
-  input_file(input_text,tokens)
+  tokens = input_text.split();
+  return tokens
