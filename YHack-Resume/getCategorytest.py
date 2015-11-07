015d82bc42df3ddb9c0ab0d96325ea4e7db5c8cb
nazliuzgur <nazliuzgur@gmail.com>
2015-11-07 16:37:49 -0500

    trying to find the problems in test cases

--- YHack-Resume/getCategorytest.py
+++ YHack-Resume/getCategorytest.py
@@ -2,12 +2,11 @@ from getCategory import *
 
 # Tests
 
-# software total phrase count -> 58
-print(softwareScore("resume"))
+# software total phrase count -> 57
 assert(softwareScore("resume") == 0.0)
 assert(softwareScore("This is a resume") == 0.0)
-assert(softwareScore("c") == (1.0/58))
-assert(softwareScore("c c++ c#") == (3.0/58))
+assert(softwareScore("c") == (1.0/57))
+assert(softwareScore("c c++ c#") == (3.0/57))
 
 # engineeringScore array length -> 27
 assert(engineeringScore("resume") == 0.0)
@@ -21,18 +20,18 @@ assert(financeScore("This is a resume") == 0.0)
 assert(financeScore("powerpoint") == (1.0/24))
 assert(financeScore("forecasting trend analysis team") == (3.0/24))
 
-# managementScore array length -> 25
+# managementScore array length -> 23
 assert(managementScore("resume") == 0.0)
 assert(managementScore("This is a resume") == 0.0)
-assert(managementScore("powerpoint") == (1.0/25))
-assert(managementScore("operational development firing budget") == (3.0/25))
+assert(managementScore("evaluations") == (1.0/23))
+assert(managementScore("operational development firing budget") == (3.0/23))
 
 # mainCategoryAndScore
 assert(mainCategoryAndScore("resume") == ('computer science',0.0))
 assert(mainCategoryAndScore("This is a resume") == ('computer science',0.0))
-assert(mainCategoryAndScore("powerpoint") == ('management',(1.0/25)))
-assert(mainCategoryAndScore("operational development c firing budget engineering") == ('finance',(3.0/25)))
-assert(mainCategoryAndScore("sml c firing budget engineering") == ('computer science',(2.0/58)))
+assert(mainCategoryAndScore("powerpoint") == ('finance',(1.0/24)))
+assert(mainCategoryAndScore("operational development c firing budget engineering") == ('business management',(3.0/23)))
+assert(mainCategoryAndScore("sml c firing budget engineering") == ('business management',(2.0/23)))
 assert(mainCategoryAndScore("build sml hiring engineering") == ('engineering',(2.0/27)))
 
 
